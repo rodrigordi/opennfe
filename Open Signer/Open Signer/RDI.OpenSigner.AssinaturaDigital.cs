9436030d23b6906fe20a63db5165d4ca70bbfc5b
Rodrigo Rocha <rodrigo@rochadigital.com>
2018-04-05 12:56:56 -0300

    Correção algoritmo assinatura eSocial. SHA256

--- Open Signer/Open Signer/RDI.OpenSigner.AssinaturaDigital.cs
+++ Open Signer/Open Signer/RDI.OpenSigner.AssinaturaDigital.cs
@@ -640,12 +640,11 @@ namespace RDI.OpenSigner
                                 // adiciona a chave do certificado
                                 signedXml.SigningKey = _X509Cert.PrivateKey;
                                 //definir explicitamente o metodo
-                                signedXml.SignedInfo.SignatureMethod = "http://www.w3.org/2000/09/xmldsig-more#rsa-sha256";
-
+                                signedXml.SignedInfo.SignatureMethod = "http://www.w3.org/2001/04/xmldsig-more#rsasha256";
                                 // Cria a referencia para assinatura
                                 Reference reference = new Reference();
                                 //definir explicitamente o metodo
-                                reference.DigestMethod = "http://www.w3.org/2000/09/xmlenc#sha256";
+                                reference.DigestMethod = "http://www.w3.org/2001/04/xmlenc#sha256";
 
                                 // pega o uri que deve ser assinada
                                 XmlAttributeCollection _Uri = doc.GetElementsByTagName(RefUri).Item(0).Attributes;
@@ -757,12 +756,11 @@ namespace RDI.OpenSigner
                                 // adiciona a chave do certificado
                                 signedXml.SigningKey = _X509Cert.PrivateKey;
                                 //definir explicitamente o metodo
-                                signedXml.SignedInfo.SignatureMethod = "http://www.w3.org/2000/09/xmldsig-more#rsa-sha256";
-
+                                signedXml.SignedInfo.SignatureMethod = "http://www.w3.org/2001/04/xmldsig-more#rsasha256";
                                 // Cria a referencia para assinatura
                                 Reference reference = new Reference();
                                 //definir explicitamente o metodo
-                                reference.DigestMethod = "http://www.w3.org/2000/09/xmlenc#sha256";
+                                reference.DigestMethod = "http://www.w3.org/2001/04/xmlenc#sha256";
 
                                 // pega o uri que deve ser assinada
                                 XmlAttributeCollection _Uri = doc.GetElementsByTagName(RefUri).Item(0).Attributes;
